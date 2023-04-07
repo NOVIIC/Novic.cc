0023f88f408afe915ad2b73a3497c39e3633e4ab
Novic_C <MinecraftNovicC@outlook.com>
2023-04-07 21:51:04 +0800

    test header

--- src/componments/header.js
+++ src/componments/header.js
@@ -1,4 +1,22 @@
-import React from "react"
+import * as React from "react"
+import { Link } from "gatsby"
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
 
-const Main = () => <h1>Header</h1>
-export default Main
+const main = () => (
+    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <header style={{ marginBottom: `1.5rem` }}>
+      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+        <h3 style={{ display: `inline` }}>NovicNet</h3>
+      </Link>
+      <ul style={{ listStyle: `none`, float: `right` }}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/page2/">Test</ListLink>
+      </ul>
+    </header>
+  </div>
+)
+export default main
