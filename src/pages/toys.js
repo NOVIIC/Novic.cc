5455920d8f3f40fcdecaad2f0987ea9d36aa3cb7
Novic-C <121367544+Novic-C@users.noreply.github.com>
2023-06-11 21:55:23 +0800

    简化代码-Links

--- src/pages/toys.js
+++ src/pages/toys.js
@@ -1,14 +1,20 @@
 import * as React from 'react'
 import Layout from "../componments/layout"
 
+const Links = props => (
+  <p>
+    <a target="_blank" rel="noreferrer" href={props.href}>{props.children}</a>
+  </p>
+)
+
 const main = () => (
   <div>
     <title>NovicNet - toys</title>
     <Layout>
-      <p>test</p>
-      <p><a target="_blank" rel="noreferrer" href='https://bing.novic.cc'>BingAI</a></p>
-      <p><a target="_blank" rel="noreferrer" href='https://unlockmusic.novic.cc/'>UnlockMusic</a></p>
-      <p><a target="_blank" rel="noreferrer" href='https://hack.chat/?novicnet'>NovicNet HackChat Room (External Link)</a></p>
+      <Links href='https://bing.novic.cc'>BingAI</Links>
+      <Links href='https://unlockmusic.novic.cc/'>UnlockMusic</Links>
+      <p>External Links</p>
+      <Links href='https://hack.chat/?novicnet'>NovicNet HackChat Room</Links>
     </Layout>
   </div>
 )
